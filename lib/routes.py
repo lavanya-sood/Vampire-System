16c6b1d24da26a6f29f619aee1ebeefce7fab6f9
Lavanya Sood <z5208121@student.unsw.edu.au>
2019-11-05 17:27:59 +1100

    Added login page

--- lib/routes.py
+++ lib/routes.py
@@ -8,21 +8,37 @@ import os
 
 @app.route('/')
 def welcome():
-    session['url'] = url_for('inventory')
+    session['url'] = url_for('welcome')
     return render_template("welcome.html")
 
-#inventory
-@app.route('/inventory', methods=['POST', 'GET'])
-def inventory():
+# #inventory
+# @app.route('/inventory', methods=['POST', 'GET'])
+# def inventory():
 
-        if (System().check_login() == False):
-            session['url'] = url_for('inventory')
-            remessy = "You were redirected to login"
-            return redirect(url_for('login',remess=remessy))
-    return render_template('inventory.html')
+#         if (System().check_login() == False):
+#             session['url'] = url_for('inventory')
+#             remessy = "You were redirected to login"
+#             return redirect(url_for('login',remess=remessy))
+#     return render_template('inventory.html')
 
-@app.route('/login/<remess>', methods=['GET', 'POST'])
-def login(remess):
+# @app.route('/login/<remess>', methods=['GET', 'POST'])
+# def login(remess):
+#     message = ""
+#     email = ""
+#     if request.method == 'POST':
+#         email = request.form["email"]
+#         password = request.form["password"]
+#         message = System().check_user(email, password)
+#         if message is "":
+#             if 'url' in session:
+#                 return redirect(session['url'])
+#             return redirect(url_for('inventory', check = check))
+
+#         return render_template("login.html", message=message,remessa="Welcome to the login page")
+#     return render_template("login.html", message=message, remessa=remess)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     message = ""
     email = ""
     if request.method == 'POST':
@@ -34,56 +50,56 @@ def login(remess):
                 return redirect(session['url'])
             return redirect(url_for('inventory', check = check))
 
-        return render_template("login.html", message=message,remessa="Welcome to the login page")
-    return render_template("login.html", message=message, remessa=remess)
+        return render_template("login.html", message=message)
+    return render_template("login.html", message=message)
 
-@app.route('/delivered')
-def delivered():
-    return render_template("delivered.html")
+# @app.route('/delivered')
+# def delivered():
+#     return render_template("delivered.html")
 
-@app.route('/warning')
-def warning():
-    return render_template("warning.html")
+# @app.route('/warning')
+# def warning():
+#     return render_template("warning.html")
 
-@app.route('/requests')
-def requests():
-    return render_template("requests.html")
+# @app.route('/requests')
+# def requests():
+#     return render_template("requests.html")
 
-@app.route('/logout')
-def logout():
-    CurrentUser = System().get_username()
-    message = ""
-    message = System().logout_user()
-    return redirect(url_for('login',remess="Welcome back to the login page"))
+# @app.route('/logout')
+# def logout():
+#     CurrentUser = System().get_username()
+#     message = ""
+#     message = System().logout_user()
+#     return redirect(url_for('login',remess="Welcome back to the login page"))
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    return render_template("signup.html")
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     return render_template("signup.html")
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    return render_template("searchResults.html")
+# @app.route('/search', methods=['GET', 'POST'])
+# def search():
+#     return render_template("searchResults.html")
 
 
-@app.route('/otherProfile/<user>', methods = ['POST', 'GET'])
-def otherProfile(user):
-    if (System().check_login() == False):
-        session['url'] = url_for('otherProfile', user = user)
-        remessy = "You were redirected to login"
-        return redirect(url_for('login',remess=remessy))
-    User = System().get_user(user)
-    CurrentUser = System().get_username()
-    return render_template("otherProfile.html", User = User)
+# @app.route('/otherProfile/<user>', methods = ['POST', 'GET'])
+# def otherProfile(user):
+#     if (System().check_login() == False):
+#         session['url'] = url_for('otherProfile', user = user)
+#         remessy = "You were redirected to login"
+#         return redirect(url_for('login',remess=remessy))
+#     User = System().get_user(user)
+#     CurrentUser = System().get_username()
+#     return render_template("otherProfile.html", User = User)
 
-@app.route('/profile', methods = ['POST', 'GET'])
-def profile():
-    if (System().check_login() == False):
-        session['url'] = url_for('profile')
-        remessy = "You were redirected to login"
-        return redirect(url_for('login',remess=remessy))
-    CurrentUser = System().get_username()
-    user = System().get_user(CurrentUser)
-    return render_template("profile.html", user = user)
+# @app.route('/profile', methods = ['POST', 'GET'])
+# def profile():
+#     if (System().check_login() == False):
+#         session['url'] = url_for('profile')
+#         remessy = "You were redirected to login"
+#         return redirect(url_for('login',remess=remessy))
+#     CurrentUser = System().get_username()
+#     user = System().get_user(CurrentUser)
+#     return render_template("profile.html", user = user)
 
 
 #end
